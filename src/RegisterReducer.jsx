850af50ad40c20a2d26ead3cd077b1e88a57c43e
Satish <89683535+softdevsatish@users.noreply.github.com>
2026-05-13 17:26:10 +0530

    implement swicth case in reducer

--- src/RegisterReducer.jsx
+++ src/RegisterReducer.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 const initialState = {
   id: "",
   name: "",
@@ -6,22 +6,44 @@ const initialState = {
   password: "",
 };
 const reducer = (currentState, action) => {
-  return {
-    ...currentState,
-    [action.fieldName]: action.fieldValue,
-  };
+  // return {
+  //   ...currentState,
+  //   [action.fieldName]: action.fieldValue,
+  // };
+  switch (action.type) {
+    case "UPDATE_FIELD":
+      return {
+        ...currentState,
+        [action.payload.fieldName]: action.payload.fieldValue,
+      };
+    case "RESET_FORM":
+      return initialState;
+    default:
+      return currentState;
+  }
 };
 
 function RegisterReducer() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [user, setUser] = useState([]);
   const handleChange = (event) => {
     const fieldName = event.target.name;
     const fieldValue = event.target.value;
     // console.log(fieldName);
     // console.log(fieldValue);
     dispatch({
-      fieldName: fieldName,
-      fieldValue: fieldValue,
+      type: "UPDATE_FIELD",
+      payload: {
+        fieldName: fieldName,
+        fieldValue: fieldValue,
+      },
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUser([...user, state]);
+    dispatch({
+      type: "RESET_FORM",
     });
   };
   return (
@@ -32,47 +54,58 @@ function RegisterReducer() {
           <input
             type="number"
             name="id"
+            value={state.id}
             placeholder="Id"
             onChange={handleChange}
           />
           <input
             type="text"
             name="name"
+            value={state.name}
             placeholder="Name"
             onChange={handleChange}
           />
           <input
             type="email"
             name="email"
+            value={state.email}
             placeholder="Email"
             onChange={handleChange}
           />
           <input
             type="password"
             name="password"
+            value={state.password}
             placeholder="Password"
             onChange={handleChange}
           />
-          <button className="register-btn">Register</button>
+          <button onClick={handleSubmit} className="register-btn">
+            Register
+          </button>
         </form>
-
-        <div className="user-list">
-          <h3>Registered Users:</h3>
-          <div className="user-card">
-            <p>
-              <strong>Id:{state.id}</strong>
-            </p>
-            <p>
-              <strong>Name:{state.name}</strong>
-            </p>
-            <p>
-              <strong>Email:{state.email}</strong>
-            </p>
-            <p>
-              <strong>Password:{state.password}</strong>
-            </p>
+        {user.length > 0 && (
+          <div className="user-list">
+            <h3>Registered Users:</h3>
+            {user.map((item, index) => {
+              return (
+                <div className="user-card" key={index}>
+                  <p>
+                    <strong>Id:{item.id}</strong>
+                  </p>
+                  <p>
+                    <strong>Name:{item.name}</strong>
+                  </p>
+                  <p>
+                    <strong>Email:{item.email}</strong>
+                  </p>
+                  <p>
+                    <strong>Password:{item.password}</strong>
+                  </p>
+                </div>
+              );
+            })}
           </div>
-        </div>
+        )}
       </div>
     </>
   );
